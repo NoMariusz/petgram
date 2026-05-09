1d4d43e13a410d83c686ddd4b69e263425c704ce
NoMariusz <mariusz2727@vp.pl>
2026-05-09 14:00:59 +0200

    spring boot app init

---
+++ backend/petgram-backend/src/main/java/com/petgram/petgrambackend/PetgramBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petgram.petgrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgramBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetgramBackendApplication.class, args);
+    }
+
+}
---
+++ backend/petgram-backend/src/test/java/com/petgram/petgrambackend/PetgramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petgram.petgrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetgramBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
